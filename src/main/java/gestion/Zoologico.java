f8987da8f781cc90cd4764b8d406e7d14b6e900f
IThiagoRo <isrojasm@unal.edu.co>
2020-09-29 17:33:34 -0500

    solution

---
+++ src/main/java/gestion/Zoologico.java
@@ -0,0 +1,50 @@
+package gestion;
+
+import java.util.ArrayList;
+
+public class Zoologico {
+	private String nombre;
+	private String ubicacion;
+	public ArrayList<Zona> zonas = new ArrayList<>();
+	
+	public Zoologico(String nombre, String ubicacion) {
+		this.nombre = nombre;
+		this.ubicacion = ubicacion;
+	}
+	
+	public Zoologico() {
+		super();
+	}
+	
+	public int cantidadTotalAnimales() {
+		int nAnimals = 0;
+		for(Zona z:zonas) {
+			nAnimals = nAnimals + z.cantidadAnimales();
+		}
+
+		return nAnimals;
+	}
+	
+	public void agregarZonas(Zona zona) {//revisar
+		zonas.add(zona);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	
+	
+
+}
