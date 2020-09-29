7658f01f7fefa5d2c411fad1dc0fc1707bf7bdb3
IThiagoRo <isrojasm@unal.edu.co>
2020-09-29 17:49:38 -0500

    solution2

--- src/main/java/zooAnimales/Pez.java
+++ src/main/java/zooAnimales/Pez.java
@@ -34,7 +34,7 @@ public class Pez extends Animal{
 		return miSalmon;
 	}
 	
-	public static Pez CrearBacalao(String nombre, int edad, String genero) {
+	public static Pez crearBacalao(String nombre, int edad, String genero) {
 		Pez miBacalao = new Pez(nombre, edad, "oceano", genero, "gris", 6);
 		bacalaos++;
 		return miBacalao;
